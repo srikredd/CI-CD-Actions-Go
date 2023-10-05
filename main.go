69140646dc99d23c43e681a25459973c2e570523
srikredd <kundurusrikanthreddy9@gmail.com>
2023-10-05 12:41:59 +0530

    Sample web app code was written

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+    "fmt"
+    "net/http"
+)
 
 func main() {
-	fmt.Println("Hello World")
+    http.HandleFunc("/", helloHandler)
+    port := 8080
+    addr := fmt.Sprintf(":%d", port)
+    fmt.Printf("Server is listening on port %d...\n", port)
+    err := http.ListenAndServe(addr, nil)
+    if err != nil {
+        fmt.Printf("Error: %v\n", err)
+    }
+}
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "Hello, World!")
 }
